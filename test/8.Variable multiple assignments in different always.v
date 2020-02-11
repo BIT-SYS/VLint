e91953e9999523998d430403621b4e3564bcaa8d
zhouzhi321 <42953327+zhouzhi321@users.noreply.github.com>
2020-02-11 15:33:27 +0800

    Add files via upload

---
+++ test/8.Variable multiple assignments in different always.v
@@ -0,0 +1,37 @@
+module top
+  (
+   input CLK,
+   input RST,
+   input enable,
+   input [31:0] value,
+   output [7:0] led
+  );
+  reg [31:0] count;
+  reg [7:0] state;
+  assign led = count[23:16];
+  
+  always @(posedge CLK)
+  begin
+    if(RST)
+      count <= CLK ;
+    else
+      count <= 1 ;
+  end
+  
+  always@(posedge CLK or negedge RST)
+  begin
+    if(RST)
+      count <= CLK ;
+    else
+      state <= 1 ;
+  end
+  
+  always@(posedge CLK or negedge RST)
+  begin
+    if(RST)
+      state <= CLK ;
+    else
+      count <= 1 ;
+  end
+  
+endmodule
