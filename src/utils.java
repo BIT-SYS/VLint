0c68090376c3444c244e5821d0ba14c232cdb378
zhouzhi321 <42953327+zhouzhi321@users.noreply.github.com>
2020-02-24 23:14:49 +0800

    Update utils.java

--- src/utils.java
+++ src/utils.java
@@ -47,6 +47,28 @@ public class utils {
         return line.substring( line.indexOf(":")+2 , line.lastIndexOf("(")-1 ) ;
     }
 
+
+    /**
+     * 抽取line中的定义变量名称
+     * @param line
+     * @return
+     */
+    public static String record_define_variable( String line )
+    {
+        return line.substring( line.indexOf(":")+2 , line.lastIndexOf(",") ) ;
+    }
+
+    /**
+     * 抽取line中的符号名称
+     * @param line
+     * @return
+     */
+    public static String record_symbol( String line )
+    {
+        return line.substring( line.indexOf(":")-2 , line.indexOf(":") ) ;
+    }
+
+
     /**
      * 去重
      * @param list1
@@ -63,6 +85,29 @@ public class utils {
         return newList ;
     }
 
+
+    /**
+     * 判断是否含有重复元素
+     * @param list1
+     * @return
+     */
+    public static boolean if_duplicate(ArrayList<String> list1)
+    {
+        ArrayList<String> newList = new ArrayList<String>();
+        boolean result = false;
+        for (String cd : list1) {
+            if (!newList.contains(cd)) {
+                newList.add(cd);
+            }
+            else
+            {
+                result = true;
+            }
+        }
+        return result;
+    }
+
+
     /**
      * 打印列表
      * @param list
@@ -78,7 +123,10 @@ public class utils {
         System.out.println("]");
     }
 
-
+    /**
+     * 找出重复元素
+     * @param pos, neg
+     */
     public static ArrayList<String> find_same(ArrayList<String> pos, ArrayList<String> neg)
     {
         ArrayList<String> result = new ArrayList<String>();
@@ -89,6 +137,49 @@ public class utils {
         }
         return result;
     }
+
+    /**
+     * 抽取IntConst行中的数字
+     * @param line
+     * @return
+     */
+    public static int intconst_num( String line )
+    {
+        String num =  line.substring( line.indexOf(":")+2 , line.lastIndexOf("(")-1 ) ;
+        int i = 0;
+        int index = num.indexOf('\'');
+        if (num.length() > 2 && index != -1)
+        {
+            num = num.substring(index + 1);
+            if (num.charAt(0) == 'h')
+            {
+                num = num.substring(1);
+                i = Integer.parseInt(num , 16);
+            }
+            else if (num.charAt(0) == 'o')
+            {
+                num = num.substring(1);
+                i = Integer.parseInt(num , 8);
+            }
+            else if (num.charAt(0) == 'b')
+            {
+                num = num.substring(1);
+                i = Integer.parseInt(num , 2);
+            }
+            return i;
+        }
+        else
+        {
+            i = Integer.parseInt( num );
+            return i;
+        }
+    }
+
+
+
+
+
+
 
 
 }
