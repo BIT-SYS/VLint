061afef9d0e9d263252fd7e0e1d9d89c628ab4ce
zhouzhi321 <42953327+zhouzhi321@users.noreply.github.com>
2020-06-18 15:49:58 +0800

    Update utils.java

--- src/utils.java
+++ src/utils.java
@@ -1,10 +1,7 @@
-import org.omg.PortableInterceptor.INACTIVE;
-
 import java.io.*;
 import java.util.ArrayList;
 
 public class utils {
-
     /**
      * 计数line中前置的空格(缩进)数量
      * @param line
@@ -148,7 +145,7 @@ public class utils {
         String num =  line.substring( line.indexOf(":")+2 , line.lastIndexOf("(")-1 ) ;
         int i = 0;
         int index = num.indexOf('\'');
-        if (num.length() > 2 && index != -1)
+        if (num.length() > 2 && index != -1 && num.length() < 16)
         {
             num = num.substring(index + 1);
             if (num.charAt(0) == 'h')
@@ -161,12 +158,26 @@ public class utils {
                 num = num.substring(1);
                 i = Integer.parseInt(num , 8);
             }
-            else if (num.charAt(0) == 'b')
+            else  if (num.charAt(0) == 'd')
             {
                 num = num.substring(1);
-                i = Integer.parseInt(num , 2);
+                i = Integer.parseInt(num, 10);
+            }
+            else if (num.charAt(0) == 'b')
+            {
+                if (num.contains("x") || num.contains("Z"))
+                    return -1;
+                else
+                {
+                    num = num.substring(1);
+                    i = Integer.parseInt(num , 2);
+                }
             }
             return i;
+        }
+        else if(num.length() > 16)
+        {
+            return -2;
         }
         else
         {
@@ -176,7 +187,61 @@ public class utils {
     }
 
 
+    /**
+     * 抽取IntConst行中的位数
+     * @param line
+     * @return
+     */
+    public static int intconst_width( String line )
+    {
+        String num =  line.substring( line.indexOf(":")+2 , line.lastIndexOf("(")-1 ) ;
+        int i = 0;
+        int index = num.indexOf('\'');
+        if (num.length() > 2 && index != -1)
+        {
+            num = num.substring(0, index);
+            i = Integer.parseInt(num);
+            return i;
+        }
+        else return Integer.parseInt(num);
+    }
 
+    /**
+     * find the tree node
+     * @param node, level, string type, line number
+     * @return the aim node
+     */
+    public static treeNode find_node(treeNode node , int level , String str_type , int line_num)
+    {
+        treeNode return_node = new treeNode();
+        if( node.childList.size()>0 )
+        {
+            for( treeNode childnode : node.childList )
+            {
+                //System.out.println(line_num);
+                if ( childnode.line == line_num && childnode.type.equals(str_type))
+                {
+                    return childnode;
+                }
+                else {
+                    return_node = find_node(childnode,level+2,str_type,line_num);
+                    if( return_node.line!=0 )
+                    {
+                        return return_node ;
+                    }
+                }
+            }
+        }
+        return return_node;
+    }
 
+    /**
+     * 判断是否是2的倍数
+     * @param n
+     * @return
+     */
+    public static boolean is_two_power(int n) {
+        return n>0 && (n&(n-1)) == 0;
+    }
 
 }
